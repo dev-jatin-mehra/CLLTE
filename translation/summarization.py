f50e4ba455daf1f9cc344d48671a0c5807e1868e
Jatin Mehra <dev.jatin.singh@gmail.com>
2024-11-22 15:50:29 +0530

    main

--- translation/summarization.py
+++ translation/summarization.py
@@ -1,5 +1,16 @@
-from transformers import pipeline
+# from transformers import pipeline
+
+# def summarize_text(text):
+#     summarizer = pipeline("summarization")
+#     return summarizer(text,max_length=130,min_length=30,do_sample=False)[0]["summary_text"]
+
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 
 def summarize_text(text):
-    summarizer = pipeline("summarization")
-    return summarizer(text,max_length=130,min_length=30,do_sample=False)[0]["summary_text"]
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+    summary = summarizer(parser.document, sentences_count=3)
+    return " ".join([str(sentence) for sentence in summary])
