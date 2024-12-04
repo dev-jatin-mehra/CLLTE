ecb23376840e85d87b1407b2250a620048c9e722
Jatin Mehra <dev.jatin.singh@gmail.com>
2024-12-04 13:49:07 +0530

    changes By dinesh

--- download/file_utils.py
+++ download/file_utils.py
@@ -9,10 +9,7 @@ from fpdf import FPDF
 def save_to_pdf(text, filename):
     pdf = FPDF()
     pdf.add_page()
-    # Use built-in Helvetica font (no need to download any font files)
     pdf.set_font("Helvetica", size=12)
-    # If needed, encode text to handle special characters
     encoded_text = text.encode('latin-1', 'replace').decode('latin-1')
-    # Write the text to the PDF
     pdf.multi_cell(0, 10, encoded_text)
     pdf.output(filename)
